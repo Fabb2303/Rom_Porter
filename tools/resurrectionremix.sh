e94cfa8d76fcbbe970a671424f4785e95191f641
Fabb2303 <windows.fabio@outlook.it>
2016-05-19 17:15:56 +0200

    Fix Resurrectionremix.sh

--- tools/resurrectionremix.sh
+++ tools/resurrectionremix.sh
@@ -1,5 +1,41 @@
 #!/bin/bash
 
-clear 
+clear
+echo "====================================="
+echo "RESURRECTION REMIX"
+echo "====================================="
+echo ""
+echo " 1) Download Source code "
+read rr
 
-echo "Ciao"
+case "$rr" in
+    "1")
+     scode
+        ;;
+*)
+        echo "Error"
+        ;; 
+esac
+
+function scode()
+{
+clear
+echo "======================================"
+echo "PORT RESURRECTION REMIX"
+echo "======================================"
+echo ""
+
+	mkdir ~/bin
+	PATH=~/bin:$PATH
+	curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+	chmod a+x ~/bin/repo
+echo ""
+echo ""
+echo "Enter The name of the folder where you download the source code"
+read folder
+mkdir ~/$folder
+cd ~/$folder
+read
+exit
+}
+
